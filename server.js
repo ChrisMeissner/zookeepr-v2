256f5ced16b9ede1c9801cd46b9e29d7d45049be
Chris Meissner <chrismeissner@Chriss-MBP.lan>
2021-01-20 11:55:56 -0800

    naming port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
+// make your port and enviornment variable 
+const PORT = process.env.PORT || 3001;
+
 // instantiate the server
 const app = express();
 
@@ -41,6 +44,6 @@ app.get('/api/animals', (req, res) => {
 });
 
 //use a method to make the server listen
-app.listen(3001, () => {
-  console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}`);
 });
